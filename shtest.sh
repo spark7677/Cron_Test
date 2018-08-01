eae70b7ef2499689204caa8bf003e9b0d727b340
spark7677 <spark7677@gmail.com>
2018-08-01 15:27:02 +0000

    EC2 Cron Build

--- shtest.sh
+++ shtest.sh
@@ -6,6 +6,8 @@ echo "########## Cron Build Time ##########"
 
 date
 
+echo ""
+echo "############# GIT PULL ##############"
 git pull
 
 echo ""
@@ -16,19 +18,25 @@ cppcheck . --xml 2> cppcheck-result.xml
 echo ""
 echo "############# Compiling ##############"
 
-
 gcc -c HelloWorld.c main.c
 gcc -o Hello HelloWorld.o main.o
 
 echo ""
 echo "########### Excuting File ############"
-
-
 ./Hello
+
+echo ""
+echo "############### GIT ADD ##############"
 
 git add .
 
+echo ""
+echo "############## GIT COMMIT ############"
+
 git commit -m "EC2 Cron Build $date"
+
+echo ""
+echo "############## GIT PUSH ##############"
 
 git push
 
